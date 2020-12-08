ea6d995a012c3d059a1b3a84af65188e458a3dd6
Rupa <rupa211984@gmail.com>
2020-12-07 17:43:34 -0800

    Add solutions and tests

--- javascript/tests/binary_tree.test.js
+++ javascript/tests/binary_tree.test.js
@@ -1,4 +1,4 @@
-const { Node } = require("../../solutions/binary_tree");
+const { Node, oneToSeven, tenToFifty, negativeToPositive } = require("../binary_tree");
 
 describe("Node", () => {
   const node = new Node(10);
@@ -23,3 +23,43 @@ describe("Node", () => {
   });
 });
     
+function bstToArray(root) {
+  if (root === null) {
+    return [];
+  }
+
+  const stack = [root];
+  const sorted = [];
+
+  while (stack.length) {
+    const node = stack[stack.length - 1];
+
+    if (node.left !== null) {
+      stack.push(node.left);
+      node.left = null;
+      continue;
+    }
+
+    sorted.push(stack.pop().value);
+
+    if (node.right !== null) {
+      stack.push(node.right);
+    }
+  }
+
+  return sorted;
+}
+
+describe("Manual BSTs", () => {
+  test("oneToSeven() returns the root node of a valid BST", () => {
+    expect(bstToArray(oneToSeven())).toEqual([1, 4, 7]);
+  });
+
+  test("tenToFifty() returns the root node of a valid BST", () => {
+    expect(bstToArray(tenToFifty())).toEqual([10, 40, 45, 46, 50]);
+  });
+
+  test("negativeToPositive()", () => {
+    expect(bstToArray(negativeToPositive())).toEqual([-20, -19, -17, -15, 0, 1, 2, 10]);
+  });
+});
--- ruby/spec/binary_tree_spec.rb
+++ ruby/spec/binary_tree_spec.rb
@@ -1,4 +1,4 @@
-require "../solutions/binary_tree"
+require "./binary_tree"
 
 describe "Node" do
   let(:node) { Node.new(10) }
@@ -19,5 +19,52 @@ describe "Node" do
 
     expect(root.left).to be(left)
     expect(root.right).to be(right)
+  end
+end
+
+def bst_to_array(root)
+  if (root == nil)
+    return []
+  end
+
+  stack = [root]
+  sorted = []
+
+  while stack.length > 0
+    node = stack.last
+
+    if (node.left != nil)
+      stack.push(node.left)
+      node.left = nil
+      next
+    end
+
+    sorted.push(stack.pop().value)
+
+    if (node.right != nil) 
+      stack.push(node.right)
+    end
+  end
+
+  sorted
+end
+
+describe "Manual BSTs" do
+  describe "one_to_seven" do
+    it "returns the root node of a valid BST" do
+      expect(bst_to_array(one_to_seven)).to eq([1, 4, 7])
+    end
+  end
+
+  describe "ten_to_fifty" do
+    it "returns the root node of a valid BST" do
+      expect(bst_to_array(ten_to_fifty)).to eq([10, 40, 45, 46, 50])
+    end
+  end
+
+  describe "negative_to_positive" do
+    it "returns the root node of a valid BST" do
+      expect(bst_to_array(negative_to_positive)).to eq([-20, -19, -17, -15, 0, 1, 2, 10])
+    end
   end
 end
